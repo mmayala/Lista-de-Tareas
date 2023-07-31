f78c058f08dde67ea3f60ee8dd376a9b8f52e615
AYALA ESPINOZA MARTHA MERCEDES <mmayalae@pichincha.com>
2023-07-31 09:53:25 -0500

    se agrega formularios reactivos

--- to-do-list/src/app/list/list.component.ts
+++ to-do-list/src/app/list/list.component.ts
@@ -1,27 +1,82 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../interfaz/task.interface';
+import { FormBuilder,FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent {
-  newTask:string = " ";
-  tasks:string[] = [];
-  isComplete:boolean=false;
 
-  constructor(){
+export class ListComponent implements OnInit {
 
+formTask: FormGroup;
+tasks:Task[] = [];
+currentId = 1;
+messageError:string = 'La lista de tareas esta vacía';
+
+  constructor(private fb:FormBuilder) {
+    this.formTask = this.fb.group({
+      newTask:['', Validators.required]
+    });
+    
+    
   }
 
-  addTask(){
-    if(this.newTask.trim() !== ""){
-      this.tasks.push(this.newTask);
-      this.newTask = "";
+  ngOnInit(): void {
+    this.getTasksLocalStorage();
+  }
+
+addTask() {
+  if(this.formTask.invalid){
+    return;
+  };
+
+  const newTask:Task = {
+    id:this.currentId++,
+    name:this.formTask.value.newTask,
+    completed:false
+  };
+  this.tasks.push(newTask);
+  this.saveLocalStorage();
+  this.formTask.reset();
+  }
+
+  get newTask(){
+    return this.formTask.get('newTask');
+  }
+
+completeItem(task:Task){
+  task.completed = !task.completed;
+  this.saveLocalStorage();
+  }
+
+saveLocalStorage(){
+  localStorage.setItem('tasks', JSON.stringify(this.tasks))
+  }
+
+getTasksLocalStorage() {
+    const dataLocal= localStorage.getItem('tasks');
+    console.log('vaya esta muy bien', dataLocal);
+    if(dataLocal){
+      this.tasks=JSON.parse(dataLocal)
+      this.currentId = this.tasks.reduce((maxId, task) => Math.max(maxId, task.id), 0) + 1;
     }
   }
-  completeItem(){
-   console.log("holaaa", this.isComplete = !this.isComplete);
+
+deletetTask(id:number){
+    this.tasks = this.tasks.filter(t => t.id !== id);
+    this.saveLocalStorage();
   }
 
+updateTasksPending():string {
+    const completedCount = this.tasks.filter(task => task.completed).length;
+    const totalCount = this.tasks.length;
+    return `${completedCount} de ${totalCount} tarea(s) completada(s)`
 }
+
+listTaskEmpty(): boolean{
+  return this.tasks.length === 0;
+  }
+}
+
